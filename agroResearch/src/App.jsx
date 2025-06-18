0373651419a81f277a9601274b74c37d9e48cf05
Shubham Parashar <shubhamparasharjind@gmail.com>
2025-06-18 16:19:52 +0530

    final

--- agroResearch/src/App.jsx
+++ agroResearch/src/App.jsx
@@ -1,15 +1,17 @@
 // import { useState } from 'react'
 // import './App.css'
-import { Outlet, Link } from 'react-router-dom'
+import { Outlet, Link, useLocation } from 'react-router-dom'
 import Navbar from './components/Navbar'
 
 function App() {
   // const [count, setCount] = useState(0)
-
+  const location = useLocation();
+  const ishome = location.pathname === "/";
   return (
     <>
       {/* <h1>chl to rha h</h1> */}
       <Navbar />
+      {ishome && <h1 className='mt-[200px] text-red-500 kuchbhi'>Use the Navbar</h1>}
       {<Outlet />}
     </>
   )
--- agroResearch/src/components/Navbar.jsx
+++ agroResearch/src/components/Navbar.jsx
@@ -4,12 +4,14 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
     return (
         <div className="fixed left-0 flex w-[100vw] items-center justify-around gap-6 p-4 bg-gray-300">
-            <h1 className="text-2xl text-blue-900 font-bold">Number Trainer</h1>
+            <Link to={"/"}>
+                <h1 className="text-2xl text-blue-900 font-bold">Number Trainer</h1>
+            </Link>
             <div className="buttons flex gap-10">
-                <Link  to={"/squares-trainer"}>
+                <Link to={"/squares-trainer"}>
                     <button className='cursor-pointer text-2xl'>Square Trainer</button>
                 </Link>
-                <Link  to={"/cubes-trainer"}>
+                <Link to={"/cubes-trainer"}>
                     <button className='cursor-pointer text-2xl'>Cube Trainer</button>
                 </Link>
             </div>
